69a51d3a02152eb7d9287cc22f28d56f2bb2c40a
Mark Nielsen <mark@moodlerooms.com>
2017-09-13 15:15:02 -0700

    INT-12069: Update release/requires for 3.3

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'logstore_caliper';
 $plugin->version = 2017063000;
-$plugin->release = '3.2.3';
-$plugin->requires = 2016120500;
+$plugin->release = '3.3.1';
+$plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
