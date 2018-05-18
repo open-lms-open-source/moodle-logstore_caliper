aea3f3c42a56e51c61e5bd1a16d0da7077acb363
Mark Nielsen <mark@moodlerooms.com>
2018-05-18 10:07:30 -0700

    INT-13304: bump release info

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'logstore_caliper';
 $plugin->version = 2017120800;
-$plugin->release = '3.3.3';
+$plugin->release = '3.3.6';
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
