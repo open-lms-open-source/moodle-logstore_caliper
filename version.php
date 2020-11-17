643f52f7ebb6e2eb36cbb022b793f2aefaf1e03b
David Castro <david.castro@openlms.net>
2020-11-17 16:34:51 -0500

    INT-16487: 3.9.3 version bump.

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'logstore_caliper';
 $plugin->version = 2020061100;
-$plugin->release = '3.8.3';
-$plugin->requires = 2019111800;
+$plugin->release = '3.9.3';
+$plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
