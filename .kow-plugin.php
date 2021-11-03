f41d268ca543c8a8560bae418b7b8a579107c027
Oscar Nadjar <oscar.nadjar@openlms.net>
2021-11-03 14:54:54 +0000

    INT-17596 logstore_caliper: Apply snapshot 2 at version 2021060100

--- .kow-plugin.php
+++ .kow-plugin.php
@@ -1,7 +1,27 @@
 <?php die(); ?>
 {
-    "branch": "logstore_caliper-0-2016061500",
+    "branch": "logstore_caliper-2-2021060100",
     "component": "logstore_caliper",
-    "snapshot": 0,
-    "version": 2016061500
+    "snapshot": 2,
+    "version": 2021060100,
+    "dbVersion": {
+        "id": 24373,
+        "version": "2021060100",
+        "release": "3.10.3",
+        "maturity": 200,
+        "downloadurl": "https:\/\/moodle.org\/plugins\/download.php\/24373\/logstore_caliper_moodle310_2021060100.zip",
+        "downloadmd5": "9229ab611fceacdcd5166dc52eae9d6b",
+        "vcssystem": "git",
+        "vcssystemother": null,
+        "vcsrepositoryurl": "https:\/\/github.com\/open-lms-open-source\/moodle-logstore_caliper",
+        "vcsbranch": null,
+        "vcstag": null,
+        "timecreated": 1622736696,
+        "supportedmoodles": [
+            {
+                "version": 2020102800,
+                "release": "3.10"
+            }
+        ]
+    }
 }
