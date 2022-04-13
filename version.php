2ae1c82776558c9432f46874d0042037eeda9e57
Jonathan Garcia <Jonathan.Garcia@openlms.net>
2022-04-13 11:05:22 -0500

    INT-17961: 3.11mp2 version bump

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'logstore_caliper';
-$plugin->version = 2021112600;
+$plugin->version = 2022041300;
 $plugin->release = '3.11.3';
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_STABLE;
