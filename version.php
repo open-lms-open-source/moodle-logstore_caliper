ec3cde329e7facc72435ff919aab2bd7d4d06d57
Julian Tovar <julings12@gmail.com>
2023-01-24 10:58:39 -0500

    INT-18643: 4.0 MP1 Version bump

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'logstore_caliper';
-$plugin->version = 2022122100;
+$plugin->version = 2023012400;
 $plugin->release = '4.0.5';
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
