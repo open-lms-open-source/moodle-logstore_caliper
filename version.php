bc889f3483031febb8785edd562c33d221a9c332
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2024-06-17 16:37:58 -0500

    INT-19886: 4.3 Version Bump of Open LMS plugins

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'logstore_caliper';
-$plugin->version = 2024040200;
-$plugin->release = '4.1.3';
-$plugin->requires = 2022112800;
+$plugin->version = 2024061700;
+$plugin->release = '4.3.4';
+$plugin->requires = 2023100900;
 $plugin->maturity = MATURITY_STABLE;
